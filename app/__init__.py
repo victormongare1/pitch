b13cdbd31c95d33cbf05c651c0d525bdf655657a
victormongare1 <vicmongz254@gmail.com>
2020-05-02 12:03:51 +0300

    initialized database in application:

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 simple=SimpleMDE()
+db=SQLAlchemy()
 def create_app(config_name):
   # Initializing application
   app = Flask(__name__)
@@ -13,6 +15,7 @@ def create_app(config_name):
   # Initializing Flask Extensions
   bootstrap.init_app (app)
   simple.init_app(app)
+  db.init_app(app)
   # Registering the blueprint
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
