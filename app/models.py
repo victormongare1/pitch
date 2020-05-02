e5fe782f5f85797630de1df04b574987f715640c
victormongare1 <vicmongz254@gmail.com>
2020-05-02 13:15:38 +0300

    added flask migrate to application

--- app/models.py
+++ app/models.py
@@ -1,12 +1,31 @@
 from . import db
 from datetime import datetime
-class User(db.model):
+class User(db.Model):
   '''
   user class to define user objects
   '''
   __tablename__='users'
-class Comment(db.model):
+  id=db.Column(db.Integer,primary_key = True)
+  username=db.Column(db.String(255),index=True)
+  email=db.Column(db.String(255),unique=True,index=True)
+  pass_secure=db.Column(db.String(255))
+  password_hash=db.Column(db.String(255))
+  bio=db.Column(db.String(255))
+  pitches=db.relationship('Pitch',backref='user',lazy="dynamic")
+  comment=db.relationship('Comment',backref='user',lazy='dynamic')
+class Pitch(db.Model):
+  '''
+  pitch class to define pitch objects
+  '''
+  __tablename__ ='pitches'
+   id = db.Column(db.Integer, primary_key = True)
+  owner_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+class Comment(db.Model):
   '''
   comment class to define comment objects
   ''' 
   __tablename__='comments'
+  id = db.Column(db.Integer,primary_key=True)
+  pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+  description = db.Column(db.String))
