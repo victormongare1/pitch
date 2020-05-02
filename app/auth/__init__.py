f0bb9fff0e66952acc9fcacdd7ec0e09d465a7c9
victormongare1 <vicmongz254@gmail.com>
2020-05-02 15:13:40 +0300

    installed flask wtf forms and created registering form and login forms

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,4 @@
 from flask import Blueprint
+from . import views,forms
 auth= Blueprint('auth',__name__)
 from . import views,forms
--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from . import db,login_manager
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
   '''
   user class to define user objects
   '''
