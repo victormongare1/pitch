91ffd5c48d6431d34c4b8c2e89c708a268e5244b
victormongare1 <vicmongz254@gmail.com>
2020-05-02 15:16:50 +0300

    addede view root for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,3 +4,16 @@ from . import auth
 @auth.route('/login')
 def login():
   return render_template('auth/login.html')
+  
+@auth.route('/register',methods=["GET","POST"])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(email=form.email.data,username=form.username.data,password=form.password.data) 
+    db.session.add(user)
+    db.session.commit()
+    mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
+    return redirect(url_for('auth.login'))
+    title="New Account"
+  return render_template('auth/register.html',registration_form=form)     
