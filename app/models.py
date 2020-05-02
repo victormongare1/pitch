1ecbb2c012f58eddc42a2d7dd51083a2d1ce62dc
victormongare1 <vicmongz254@gmail.com>
2020-05-02 13:30:38 +0300

    created the user class

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(int(user_id))
-  
+
 class User(db.Model):
   '''
   user class to define user objects
@@ -19,7 +19,20 @@ class User(db.Model):
   bio=db.Column(db.String(255))
   pitches=db.relationship('Pitch',backref='user',lazy="dynamic")
   comment=db.relationship('Comment',backref='user',lazy='dynamic')
-
+  
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
+  @password.setter
+  def password(self,password):
+    self.pass_secure=generate_password_hash(password)
+  def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password) 
+  def save_user(self):
+    db.session.add(self)
+    db.session.commit()   
+  def __repr__(self):
+    return f'User {self.username}' 
 
 class Pitch(db.Model):
   '''
